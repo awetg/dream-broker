5db71919fc33bb38c1a6e2232367b26a0c133dd1
Awet Ghebrelassie <awetghebreslassie@gmail.com>
2020-01-27 19:51:45 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+    .use(express.urlencoded({ extended: false }))
+    .post('/analyze', (req, res) => {
+        const text = req.body.text;
+        if (text && typeof text == 'string') {
+            // trim and split on one or more whitespace and take lenght
+            const wordCount = text.trim().split(/\s+/).length;
+            // whole text length
+            const textLengthWithSpace = text.length;
+            // replace all whitespace and take length
+            const textLengthWithoutSpace = text.replace(/\s+/g, '').length;
+
+            /* replace all characters which are not A-Z or a-z with empty string and convert to array of english alphabet charcters,
+            all characters are converted to lower case to avoid separate count of small and capital letters */
+            const charCount = text.replace(/[^A-Za-z]/g, "").toLocaleLowerCase().split('').reduce((acc, char) => {
+                // if character exits in accmulator take count of charater, if not take 0 and add 1
+                acc[char] = (acc[char] || 0) + 1;
+                return acc;
+            }, {});
+            // iterating over key value pair of charaterst to sort and covert them to array
+            res.status(200).json({
+                "textLength": { "withSpaces": textLengthWithSpace, "withoutSpaces": textLengthWithoutSpace },
+                "wordCount": wordCount,
+                "characterCount": Object.keys(charCount).sort().map(k => { return { [k]: charCount[k] }; })
+            });
+        } else {
+            res.status(400).json({
+                error: {
+                    message: 'Invalid request. Please check yoU have valid request. Only (POST) json data of {"text":"your text"} is supported.'
+                }
+            })
+        }
+    })
+    .use((_, res) => res.status(501).json({ error: { message: 'Please check you have the correct URL.' } }));
+
+
+module.exports = app.listen(process.env.PORT || 3000, () => console.log('Server runnig on port ' + (process.env.PORT || 3000)));
---
+++ test.js
@@ -0,0 +1,64 @@
+const supertest = require("supertest");
+const app = require("./index");
+
+// testing with valid post request
+describe("POST /", () => {
+    it("it shoud return text analyze and status code 200", done => {
+        supertest(app)
+            .post("/analyze")
+            .send({ "text": "hello 2 times  " })
+            .expect(200)
+            .expect({ "textLength": { "withSpaces": 15, "withoutSpaces": 11 }, "wordCount": 3, "characterCount": [{ "e": 2 }, { "h": 1 }, { "i": 1 }, { "l": 2 }, { "m": 1 }, { "o": 1 }, { "s": 1 }, { "t": 1 }] })
+            .end((err, res) => {
+                if (err) done(err);
+                done();
+            });
+    });
+});
+
+// testing with invalid post request, client error
+describe("POST /", () => {
+    it("it shoud return invalid request for unsupported input and status code 400", done => {
+        supertest(app)
+            .post("/analyze")
+            .send({ "text": 2 })
+            .expect(400)
+            .expect({
+                error: {
+                    message: 'Invalid request. Please check yoU have valid request. Only (POST) json data of {"text":"your text"} is supported.'
+                }
+            })
+            .end((err, res) => {
+                if (err) done(err);
+                done();
+            });
+    });
+});
+
+// testing for any unhandled paths
+describe("POST /", () => {
+    it("it shoud return invalid URL for any unhandled paths and status code 501", done => {
+        supertest(app)
+            .post("/example")
+            .send({ "text": 2 })
+            .expect(501)
+            .expect({ error: { message: 'Please check you have the correct URL.' } })
+            .end((err, res) => {
+                if (err) done(err);
+                done();
+            });
+    });
+});
+
+describe("GET /", function () {
+    it("it shoud return invalid URL for any unhandled paths and status code 501", done => {
+        supertest(app)
+            .get("/")
+            .expect(501)
+            .expect({ error: { message: 'Please check you have the correct URL.' } })
+            .end((err, res) => {
+                if (err) done(err);
+                done();
+            });
+    });
+});
